9abbd23b02d0fcb2f1e7ecb4d27aebb5f952392c
12345678yaman <alrfayyman18@gmail.com>
2025-03-09 14:17:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html><html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="stylesheet" type="text/css" href="cid:css-b36180b8-3a04-44ed-8d89-7a3e4bc919ab@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-6abb388b-8652-4df2-87da-6817b53f2969@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-8f0ce8c5-32b4-4da1-bbdc-0366f5f1ab79@mhtml.blink" /><link rel="stylesheet" type="text/css" href="cid:css-a7626536-c016-49e7-9b2b-b6fe86f49945@mhtml.blink" />
+		
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+				<link rel="icon" type="image/x-icon" href="https://algadeeriq.com/uploads/png/01ccd91235dd5d577fb6484545212374.png">	
+		<title>LIVE PLUS</title>
+		
+		
+
+			
+			
+			
+<iframe src="https://tyg6gy7.blogspot.com/" width="100%" height="2500" frameborder="0" scrolling="no" frameborder="0" marginheight="0" marginwidth="0" allowfullscreen="true" style="border:none;"></iframe>
+
+
+
+
+
+
+
+
+
+
+
+
+
